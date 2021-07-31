b50da1f364437bd7d9e6eb90fd8e6de62af1bd78
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-31 12:10:19 -0500

    Have only the printf function

--- _printf.c
+++ _printf.c
@@ -3,29 +3,6 @@
 #include <stdio.h>
 
 /**
- * get_option - Get the function based on the input value
- * @format: string that contains what we will print
- *
- * Return: corresponding pointer to function or NULL
- */
-int (*get_option(const char *format))(va_list)
-{
-	unsigned int i;
-	print_f options[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"b", print_binary},
-		{"d", print_d},
-		{"i", print_i},
-		{NULL, NULL}
-	};
-	for (i = 0; options[i].c != NULL; i++)
-		if (*options[i].c == *format)
-			break;
-	return (options[i].f);
-}
-
-/**
  * _prinf - print anything
  * @format: string that contains what we will print
  *
